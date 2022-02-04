dd83b8b786a90fa03989443c0ee351c80ae3c9e3
李鹏龙 <819508292@qq.com>
2022-02-04 19:52:03 +0800

    Go变量:已知错误

--- go-demo02/demo03.go
+++ go-demo02/demo03.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 //声明全局变量
-var c int
+var c1 int
 
 //声明全局变量
 var bb float32 = 3.14
@@ -14,8 +14,8 @@ func main() {
 	//初始化参数
 	a = 3
 	b = 4
-	c = a + b
-	fmt.Printf("a = %d, b = %d, c = %d\n", a, b, c)
+	c1 = a + b
+	fmt.Printf("a = %d, b = %d, c = %d\n", a, b, c1)
 
 	bb := 3
 	fmt.Println(bb)
