ec6808223fbcb1b66cec3a45603a1eae3e999675
李鹏龙 <819508292@qq.com>
2022-02-04 19:39:47 +0800

    Go变量:变量交换，第二种写法

--- go-demo02/demo01.go
+++ go-demo02/demo01.go
@@ -7,10 +7,16 @@ func main() {
 	var a = 100
 	var b = 200
 	// 变量交换 第一种
-	var c int
-	c = b
-	b = a
-	a = c
+	//var c int
+	//c = b
+	//b = a
+	//a = c
+	//fmt.Printf("a=%d,b=%d", a, b)
+
+	// 变量交换 第二种
+	a = a ^ b
+	b = b ^ a
+	a = a ^ b
 	fmt.Printf("a=%d,b=%d", a, b)
 
 }
