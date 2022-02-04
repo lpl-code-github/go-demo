7ec2386d78d00194ab253dec4b59f5121691665e
李鹏龙 <819508292@qq.com>
2022-02-04 19:51:06 +0800

    Go变量:全局变量与局部变量名称可以相同，但是函数体内的局部变量会被优先考虑

--- go-demo02/demo03.go
+++ go-demo02/demo03.go
@@ -5,6 +5,9 @@ import "fmt"
 //声明全局变量
 var c int
 
+//声明全局变量
+var bb float32 = 3.14
+
 func main() {
 	//声明局部变量
 	var a, b int
@@ -13,4 +16,7 @@ func main() {
 	b = 4
 	c = a + b
 	fmt.Printf("a = %d, b = %d, c = %d\n", a, b, c)
+
+	bb := 3
+	fmt.Println(bb)
 }
