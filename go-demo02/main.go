d1e47d8fadba386304adc29e63324df9d56744bb
李鹏龙 <819508292@qq.com>
2022-02-04 18:55:40 +0800

    Go变量:不指明变量类型

--- go-demo02/main.go
+++ go-demo02/main.go
@@ -2,9 +2,16 @@ package main
 
 import "fmt"
 
+// 标准格式声明变量
 var age int
+
+// 游戏角色初始等级 不指明变量类型
+var level = 1
 
 func main() {
 	// %d 代表要打印一个整数类型
-	fmt.Printf("%d", age)
+	//fmt.Printf("%d", age)
+
+	// %T 打印类型
+	fmt.Printf("%T", level)
 }
