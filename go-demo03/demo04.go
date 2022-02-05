cb7fe664225fdd9abcf76487a612ccb52474b115
李鹏龙 <819508292@qq.com>
2022-02-05 21:16:48 +0800

    变量类型：字符串型,中文三字节，字母一个字节

--- go-demo03/demo04.go
+++ go-demo03/demo04.go
@@ -14,5 +14,8 @@ golang`) //使用反引号 可以进行字符串换行
 	var mystr01 string = "hello"
 	var mystr02 [5]byte = [5]byte{104, 101, 108, 108, 111}
 	fmt.Printf("myStr01: %s\n", mystr01)
-	fmt.Printf("myStr02: %s", mystr02)
+	fmt.Printf("myStr02: %s\n", mystr02)
+
+	var myStr01 string = "hello,世界"
+	fmt.Printf("myStr01: %d\n", len(myStr01)) //输出结果 myStr01: 12
 }
