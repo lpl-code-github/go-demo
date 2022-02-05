07d7be6022212cf9ef2c81465ea36ba455d194b0
李鹏龙 <819508292@qq.com>
2022-02-05 20:50:56 +0800

    变量类型：浮点型,浮点数在声明的时候可以只写整数部分或者小数部分

--- go-demo03/demo01.go
+++ go-demo03/demo01.go
@@ -14,4 +14,9 @@ func main() {
 	fmt.Printf("%.0f,%.0f", f, f+1)
 	fmt.Println("")
 	fmt.Println(f == f+1)
+
+	// 浮点数在声明的时候可以只写整数部分或者小数部分
+	var e = .71828
+	var g = 1.
+	fmt.Printf("%.5f,%.1f", e, g) // 执行结果 0.71828,1.0
 }
