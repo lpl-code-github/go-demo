7ad70640ae2d3e838a3da22672fee3732a2ba6cd
李鹏龙 <819508292@qq.com>
2022-02-05 20:48:27 +0800

    变量类型：浮点型,float32 类型的累计计算误差很容易扩散,通常应该优先使用 float64 类型

--- go-demo03/demo01.go
+++ go-demo03/demo01.go
@@ -4,7 +4,14 @@ import "fmt"
 
 // 浮点型
 func main() {
-	floatNum := 3.2
-	// 保留小数点后2位数
-	fmt.Printf("%.2f\n", floatNum)
+	//floatNum := 3.2
+	//// 保留小数点后2位数
+	//fmt.Printf("%.2f\n", floatNum)
+
+	// float32 类型的累计计算误差很容易扩散
+	var f float32 = 1 << 24
+	fmt.Println(f)
+	fmt.Printf("%.0f,%.0f", f, f+1)
+	fmt.Println("")
+	fmt.Println(f == f+1)
 }
