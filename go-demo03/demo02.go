96d0a9b434b1cae20a28d0588d0d9d2241fecb82
李鹏龙 <819508292@qq.com>
2022-02-05 20:58:09 +0800

    变量类型：布尔类型，&&(AND),||(OR)是具有短路行为

--- go-demo03/demo02.go
+++ go-demo03/demo02.go
@@ -1,9 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
 	var aVar = 10
 	println(aVar == 5)  // false
 	println(aVar == 10) // true
 	println(aVar != 5)  // true
 	println(aVar != 10) // false
+
+	var a = 10
+	//因为a>11已经不满足了，所以a < 30不会执行，整个表达式为false
+	if a > 11 && a < 30 {
+		fmt.Println("正确")
+	} else {
+		fmt.Println("错误")
+	}
+
+	//因为a > 5已经满足了，所以a < 30不会执行，整个表达式为true
+	if a > 5 || a < 30 {
+		fmt.Println("正确")
+	} else {
+		fmt.Println("错误")
+	}
 }
