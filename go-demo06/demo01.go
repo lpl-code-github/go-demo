acfaa710faf5c15b6fd18791eaa6fe8893955175
李鹏龙 <819508292@qq.com>
2022-02-10 18:16:13 +0800

    go字符串与其他数据类型的转换:string转int.修改输出格式化

--- go-demo06/demo01.go
+++ go-demo06/demo01.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	newStr1 := "1"
 	intValue, _ := strconv.Atoi(newStr1)
-	fmt.Printf("%T,%d\n", intValue, intValue)
+	fmt.Printf("ingValue类型:%T，intValue值:%d", intValue, intValue)
 }
